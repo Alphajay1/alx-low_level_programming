7affe3254a98ec5fa63969c39050317462469d86
Alphajay1 <ayakojamesty@gmail.com>
2023-05-19 02:50:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0, Always.
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);	
+	}
+	putchar('\n');
+	return (0);
+}
