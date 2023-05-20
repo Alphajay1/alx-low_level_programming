73c7e3244d18360b1974eb8b6b4fa6a6d571a12e
Alphajay1 <ayakojamesty@gmail.com>
2023-05-19 19:50:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int main(void)
 			for (z = y + 1; z <= 9; z++)
 			{
 				putchar((x % 10) + '0');
-				putchar((y %10) + '0');
+				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
 				if (x == 7 && y == 8 && z == 9)
 					continue;
