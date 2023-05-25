1d0348390891447de6ba7691268fe582ced15d0e
Alphajay1 <ayakojamesty@gmail.com>
2023-05-25 05:52:21 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,32 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * This is a header file for my task 0x04-more_functions_nested_loops
- * and i am going to use it as my own library for my prototypes
- * for my tasks.
- */
-/* Funtion prototypes */
-
-
-/*
- * _putchar - Writes the character c to stdout
- * @c:The character to print.
- *
- * Return: On success 1.On error, -1 is returned,
- * and errno is set approprietely
- */
 int _putchar(char c);
-
-
-/*
- * _isupper - checks for uppercase character.
- * @c: The character to be checked.
- *
- * Return: 1, if c is uppercase.
- * return 0, otherwise.
- */
 int _isupper(int c);
-
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif /* MAIN_H */
