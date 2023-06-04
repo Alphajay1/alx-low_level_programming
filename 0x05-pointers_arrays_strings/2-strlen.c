041852ae55157cd3935831f25749fd4f962496d7
Alphajay1 <ayakojamesty@gmail.com>
2023-06-04 05:11:45 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be counted.
+ *
+ * Return: number of characters.
+ */
+int _strlen(char *s)
+{
+	int lenght;
+
+	lenght = 0;
+	while (lenght[s] != '\0')
+	{
+		lenght++;
+	}
+	return (lenght);
+}
