779ac9dbea9666c98e6eeee77d642dc54588157e
Alphajay1 <ayakojamesty@gmail.com>
2023-07-05 00:26:09 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,9 +15,22 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index] == 0 || str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index -1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index] == '?' || str[index] == '"' || str[index] == '(' || str[index] == ')' || str[index] == '{' || str[index] == '}')
+		if (str[index] == 0 ||
+		    str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index] == '?' ||
+		    str[index] == '"' ||
+		    str[index] == '(' ||
+		    str[index] == ')' ||
+		    str[index] == '{' ||
+		    str[index] == '}')
 			str[index] = str[index] - 32;
-		
+
 		index++;
 	}
 	return (str);
