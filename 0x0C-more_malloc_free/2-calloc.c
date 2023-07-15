fe3f67200301bd23436d1a1364b88d2116d4af69
Alphajay1 <ayakojamesty@gmail.com>
2023-07-14 21:48:47 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *mem;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,6 +20,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (mem == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		mem[i] = 0;
 
 	return (mem);
 }
