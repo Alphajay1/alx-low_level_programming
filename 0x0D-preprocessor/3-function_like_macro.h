0d81dcfa4291312f155cb922c570d0d0b493ee2a
Alphajay1 <ayakojamesty@gmail.com>
2023-07-17 05:10:22 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+/*
+ * file: 3-function_like_macro.h
+ * Desc: Its a header file that computes the absolute value of a
+ * number x. It is guarded to avoid the problem of double inclusion.
+ */
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif
